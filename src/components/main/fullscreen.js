7aa198620d242680b00943b63861cbc29cd4aa0c
Chris Kelsey <cklsymail@gmail.com>
2017-06-07 14:11:47 -0500

    stuff

--- src/components/main/fullscreen.js
+++ src/components/main/fullscreen.js
@@ -1,4 +1,5 @@
 (function(app) {
     'use strict';
     app.directive('iCKonFullscreen', function($compile) { return { restrict: 'A', templateUrl: 'fullscreen.svg' }; });
+    app.directive('iCKonFullscreen2', function($compile) { return { restrict: 'A', templateUrl: 'fullscreen-2.svg' }; });
 })(angular.module('ickons'));
