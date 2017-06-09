918fb0aea0e9a87669b223fb226cf50c498a750b
Chris Kelsey <cklsymail@gmail.com>
2017-06-08 21:02:56 -0500

    stuff

--- src/components/main/general.js
+++ src/components/main/general.js
@@ -1,5 +1,6 @@
 (function(app) {
     'use strict';
     app.directive('iCKonVr', function($compile) { return { restrict: 'A', templateUrl: 'vr.svg' }; });
+    app.directive('iCKonVrNvidia', function($compile) { return { restrict: 'A', templateUrl: 'vr_nvidia.svg' }; });
     app.directive('iCKonBinoculars', function($compile) { return { restrict: 'A', templateUrl: 'binoculars.svg' }; });
 })(angular.module('ickons'));
--- src/components/main/three-sixty.js
+++ src/components/main/three-sixty.js
@@ -1,4 +1,5 @@
 (function(app) {
     'use strict';
     app.directive('iCKonThreeSixty', function($compile) { return { restrict: 'A', templateUrl: '360.svg' }; });
+    app.directive('iCKonThreeSixtyNvidia', function($compile) { return { restrict: 'A', templateUrl: '360_2.svg' }; });
 })(angular.module('ickons'));
